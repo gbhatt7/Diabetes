079508ccac9a13ccbd2f5599622c62cb413cd854
gbhatt7 <gbhattbvb7@gmail.com>
2022-07-20 00:09:06 +0530

    Home page made

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+import worldmap from "./worldmap2.png"
+
+export default function Home() {
+    let stylehome = {
+        fontFamily: 'Tabitha'
+    }
+    return (
+        <div className='homecontainer' style={stylehome}>
+            <h1>422 milliion people</h1>
+            <h2>affected with Diabetes..</h2>
+            <img src={worldmap} />
+        </div>
+    )
+}
