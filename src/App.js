a4725fcf1d2e7b1d33acdabd45c62a942e759413
gbhatt7 <gbhattbvb7@gmail.com>
2022-08-08 23:32:46 +0530

    problems removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
-import Problems from "./Components/Problems/Problems";
 import Diet from "./Components/Diet/Diet";
 import Footer from "./Components/Footer/Footer";
 
--- src/Components/Problems/Problems.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Problems() {
-  return (
-    <div className='problemcontainer' id='problem'>
-    </div>
-  )
-}
